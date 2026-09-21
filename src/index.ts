87bed3d1138167193a0de46aac8ae252204642ed
Martin <mselgmae@gmail.com>
2026-09-21 10:05:49 +0300

    Add increaseQuantity method

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,11 @@ class Product {
     getTotalPrice() {
         return this.price * this.quantity;
 }
+
+increaseQuantity(amount: number ) {
+        this.quantity += amount;
+}
 }
 const product = new Product ("Pizza", 2, 3)
+product.increaseQuantity(5)
 console.log(product.getTotalPrice());
